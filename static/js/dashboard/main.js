88c072811d13f469ea225636595891d3484dfbd1
ficky230596 <fickyrahanubun@gmail.com>
2025-07-23 21:51:24 +0800

    perbaikan tampilan sidebar

--- static/js/dashboard/main.js
+++ static/js/dashboard/main.js
@@ -1,43 +1,49 @@
 function logout() {
-  Swal.fire({
-    position: "top",
-    text: "Anda yakin untuk logout?",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Yes",
-  }).then((result) => {
-    if (result.isConfirmed) {
-      $.removeCookie("tokenDashboard");
-      localStorage.setItem("logoutDashboard", "true");
-      window.location.reload();
-    }
-  });
+    Swal.fire({
+        position: "top",
+        text: "Anda yakin untuk logout?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes",
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.removeCookie("tokenDashboard");
+            localStorage.setItem("logoutDashboard", "true");
+            window.location.reload();
+        }
+    });
 }
 
 function hideSidebar() {
-  $(".logo h4").toggleClass("d-none");
-  $(this).toggle(function () {
-    $("body").css("grid-template-columns", "5rem 1fr 1fr");
-  });
+    $('#sidebar').toggleClass('collapsed');
+    $('.content-wrapper').toggleClass('expanded');
 }
 
-$(".nav.menu .nav-link").each(function () {
-  path = $(this).attr("href");
-  if (window.location.pathname.includes(path)) {
-    $(this).removeClass("text-white");
-    $(this).addClass("text-primary");
-    $(this).addClass("active");
-    $(this).parent().children("b").removeAttr("hidden");
-  }
-});
+$(document).ready(function() {
+    // Toggle Sidebar di Desktop
+    $('#sidebarToggle').on('click', function() {
+        hideSidebar();
+    });
 
-if (localStorage.getItem("tambahData") == "true") {
-  toastr.success("Berhasil Tambah Data");
-  localStorage.removeItem("tambahData");
-}
+    // Tandai tautan navigasi aktif
+    $(".nav .nav-link").each(function() {
+        var path = $(this).attr("href");
+        if (window.location.pathname.includes(path)) {
+            $(this).removeClass("text-white").addClass("active");
+            $(this).parent().children("b").removeAttr("hidden");
+        }
+    });
 
-$(window).on('load', function() {
-  $('#loading').hide()
+    // Tampilkan notifikasi sukses jika data ditambahkan
+    if (localStorage.getItem("tambahData") === "true") {
+        toastr.success("Berhasil Tambah Data");
+        localStorage.removeItem("tambahData");
+    }
+
+    // Sembunyikan loading spinner setelah halaman dimuat
+    $(window).on('load', function() {
+        $('.loading-overlay').addClass('hidden');
+    });
 });
